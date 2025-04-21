030e84cf9e4e47cfd1588b682c00f102afef9045
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-04-21 12:07:05 +0530

    ugiuyiu

--- components/logo/logo.vue
+++ components/logo/logo.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <img src="https://cdn-icons-png.flaticon.com/128/14102/14102925.png" alt="" >
+        <img src="~/assets/image/Logo_nkyc.png" alt="logo"  >
     </div>
 </template>
 
