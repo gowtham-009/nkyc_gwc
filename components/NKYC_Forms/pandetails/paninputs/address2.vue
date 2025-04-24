21c66cafb732519b59181cfa15e132cc5964432b
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-04-24 16:37:33 +0530

    cha

--- components/NKYC_Forms/pandetails/paninputs/address2.vue
+++ components/NKYC_Forms/pandetails/paninputs/address2.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-full">
-      <label for="pan_label" class="text-gray-500 text-lg font-normal leading-3">Address line 2 <br><span class="text-sm text-gray-500 ">(Name, Area, City, State, Pincode)</span></label>
+      <label for="pan_label" class="text-gray-500 text-lg font-normal leading-3">Address line 2 </label>
 
-      <Textarea v-model="address" varient="filled" rows="3" class="w-full" cols="10" />
+      <Textarea v-model="address" varient="filled" rows="2" placeholder="Address line2" class="w-full" cols="10" />
     </div>
   </template>
   
